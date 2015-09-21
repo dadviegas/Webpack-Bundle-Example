803acee0d721bc2627d15416098d4e26e95e6d9e
dadviegas <dadviegas@hotmail.com>
2015-09-21 21:44:01 +0100

    add point and bundle files with sourcemaps Issue #1

--- assets/lib/lib.es6.js
+++ assets/lib/lib.es6.js
@@ -4,11 +4,12 @@ require('./file2.js');
 
 import Point from './es6/point.es6.js';
 
-var point = new Point(2,3);
+var point1 = new Point(2,3);
+var point = new Point(12,23);
 
 var lib3 = {
   text: "i am lib file"
 }
 
 
-console.log(lib3.text, point);
+console.log(lib3.text, point.toString(), point1.toString());
