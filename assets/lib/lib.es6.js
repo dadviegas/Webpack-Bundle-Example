8e33ca81252a20ee9ed51476914c526bab19145f
dadviegas <dadviegas@hotmail.com>
2015-09-21 19:40:53 +0100

    Add babel loader to webpack.
    Issue #1

--- assets/lib/lib.es6.js
+++ assets/lib/lib.es6.js
@@ -2,10 +2,13 @@ require('./file3.js');
 require('./file1.js');
 require('./file2.js');
 
+import Point from './es6/point.es6.js';
+
+var point = new Point(2,3);
 
 var lib3 = {
   text: "i am lib file"
 }
 
 
-console.log(lib3.text);
+console.log(lib3.text, point);
